34f53c73db3f2176dad6959e42271cf4d63af307
PRAKASH KATARIYA <108291408+prakash-javascript@users.noreply.github.com>
2022-10-26 19:49:01 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Welcome from './Welcome';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Welcome name="prakash"/>
+    </div>
+  );
+}
+
+export default App;
